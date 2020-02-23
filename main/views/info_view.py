25d5b5810733b6068017bd13b517b9d08e053a01
sc18jt <sc18jt@leeds.ac.uk>
2020-02-23 19:14:02 +0000

    Create info_view.py functionality.

--- main/views/info_view.py
+++ main/views/info_view.py
@@ -1,3 +1,7 @@
 import flask
 
 blueprint = flask.Blueprint("info", __name__)
+
+@blueprint.route("/info/memberships", methods=["GET"])
+def membership_func():
+    return flask.render_template("info/memberships.html")
