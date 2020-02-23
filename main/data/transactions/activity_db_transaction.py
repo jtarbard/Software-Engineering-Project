439812648ddf8c2250435f8239cfa0f3d2e4eb03
Dante S-K <sc18d2s@leeds.ac.uk>
2020-02-23 19:53:50 +0000

    Created classes.html and added corresponding view

--- main/data/transactions/activity_db_transaction.py
+++ main/data/transactions/activity_db_transaction.py
@@ -170,3 +170,9 @@ def create_new_activity_type(name: str, description: str, category: str, tags_li
     session.commit()
     session.close()
     return True
+
+
+def return_activities_between_dates(start_date: datetime.datetime, end_time: datetime.datetime):
+    session = db.create_session()
+    print()
+    return session.query(Activity).filter(Activity.start_time > start_date, Activity.end_time < end_time)
--- main/views/activities_view.py
+++ main/views/activities_view.py
@@ -1,3 +1,28 @@
 import flask
+import main.data.transactions.activity_db_transaction as adf
+import main.data.transactions.user_db_transaction as udf
+import datetime
 
 blueprint = flask.Blueprint("activities", __name__)
+
+
+@blueprint.route("/activities/view_classes", methods=["GET"])
+def view_classes():
+    account_id = udf.check_valid_account_cookie(flask.request)  # Returns user ID from cookie
+    user = None
+    if account_id:
+        user = udf.return_user(account_id)  # Checks that the customer is in the database
+        if not user:  # If the customer is not in the database, then that customer has been deleted, but the user
+            # still has an account cookie, therefore the cookie must be destroyed as it is no longer valid
+            response = flask.redirect('/account/login')
+            udf.destroy_cookie(response)
+
+    activity_list = adf.return_activities_between_dates(datetime.datetime.now(), datetime.datetime.now()+datetime.timedelta(days=14))
+    return flask.render_template("/activities/classes.html", User=user, list=activity_list)
+
+
+@blueprint.route("/transaction/view_receipt", methods=["POST, GET"])
+def view_receipt():
+    form = flask.request
+    activity_id = form.get('activity')
+    return flask.render_template("/activities/receipt.html", User=user)
