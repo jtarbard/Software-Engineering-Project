2d7f254f15311e4b63966fdaabc4a8d7b304367d
Lewis Stuart <Lewis.stuart11@yahoo.co.uk>
2020-03-11 15:27:13 +0000

    implemented qrcode creation and convertion to a receipt page

--- main/views/transaction_view.py
+++ main/views/transaction_view.py
@@ -1,12 +1,15 @@
+import os
+from pyzbar.pyzbar import decode
+from PIL import Image
 import flask
 import datetime
+from werkzeug.utils import secure_filename
 import cryptography
 import main.data.transactions.activity_db_transaction as adf
 import main.data.transactions.user_db_transaction as udf
 import main.data.transactions.transaction_db_transaction as tdf
 import main.cookie_transaction as ct
 from main.data.db_classes.transaction_db_class import Receipt
-
 blueprint = flask.Blueprint("transaction", __name__)
 
 
@@ -49,14 +52,50 @@ def card_payment_post():
     return response
 
 
+@blueprint.route("/transactions/receipts/check_receipt_input", methods=["POST"])
+def check_receipt_qr_code():
+    user, response = ct.return_user_response(flask.request, True)
+    if response:
+        return response
+
+    if 'file' not in flask.request.files:
+        return flask.redirect("/account/your_account")
+
+    receipt_file = flask.request.files["file"]
+
+    if len(receipt_file.filename) < 1:
+        return flask.redirect("/account/your_account")
+
+    if receipt_file and receipt_file.filename.rsplit(".", 1)[1].lower() in ["png","jpg","jpeg"]:
+
+        file_direct = os.path.join("tmp", str(user.user_id) + datetime.datetime.now().strftime("-%m-%d-%Y-%H-%M-%S")\
+                                   + "." + receipt_file.filename.rsplit(".", 1)[1].lower())
+        receipt_file.save(file_direct)
+
+        try:
+            data = decode(Image.open(file_direct))
+            encrypted_receipt = data[0][0].decode("utf-8")
+        except:
+            return flask.redirect("/account/your_account")
+
+        os.remove(file_direct)
+        response = flask.redirect(f"/transactions/receipts/{encrypted_receipt}")
+        return response
+
+    else:
+        return flask.redirect("/account/your_account")
+
+
 @blueprint.route("/transactions/receipts/<path:encrypted_receipt>")
 def receipt_get(encrypted_receipt: str):
     user, response = ct.return_user_response(flask.request, True)
     if response:
         return response
-    returned_receipt: Receipt = tdf.check_encrypted_receipt(encrypted_receipt, user)
-    if not returned_receipt:
+    try:
+        returned_receipt: Receipt = tdf.check_encrypted_receipt(encrypted_receipt, user)
+    except:
         return flask.abort(404)
     else:
-        return flask.render_template("/transactions/receipt.html", returned_receipt=returned_receipt)
+        return flask.render_template("/transactions/receipt.html", returned_receipt=returned_receipt,
+                                     encrypted_receipt=encrypted_receipt)
 
