2319fee415bfe69ed1b6fb6836bb90c37bb7d475
Chan Wai Lou <sc18cwl@leeds.ac.uk>
2020-03-18 12:17:44 +0000

    Add primitive & naive page loading test

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,12 +5,22 @@ import unittest
 # Yoinked from www.youtube.com/watch?v=1aHNs1aEATg
 class FlaskTestCase(unittest.TestCase):
 
-    # Ensure flask was set up correctly
+    # Ensure flask was set up correctly and the website runs
     def test_index(self):
         tester = flask_app.test_client(self)
-        response = tester.get('/login', content_type='html/text')
+        response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+
+    # Ensure the membership page loads correctly
+    # Note: this is really primitive...
+    # TODO: Find some way that extracts the title & content of a page. What is the criteria that defines a page loads correctly?
+    def test_membership(self):
+        tester = flask_app.test_client(self)
+        response = tester.get('/info/memberships', content_type='html/text')
+        print(response.data) # debug. learning what response data contains
+        self.assertIn(b'Memberships', response.data)
 
 
 if __name__ == '__main__':
     unittest.main()
+
