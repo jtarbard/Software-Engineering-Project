0470b83da15418a2beeace829c80e1f722dcadf9
VincentLou <sc18cwl@leeds.ac.uk>
2020-03-28 03:06:24 +0000

    Implement basic test

--- tests/functional/misc/test_policy_info.py
+++ tests/functional/misc/test_policy_info.py
@@ -1,5 +1,17 @@
+from bs4 import BeautifulSoup
 
 
-"""
-<Rule '/misc/policy_info' (OPTIONS, HEAD, GET) -> misc.policy_info_view>
-"""
+def test_policy_info_get(test_client, page_title_dict):
+    """
+    GIVEN a Flask application
+    WHEN the '/misc/policy_info' page is requested (GET)
+    THEN check the response is valid
+
+    TESTING FOR <Rule '/misc/policy_info' (OPTIONS, HEAD, GET) -> misc.policy_info_view>
+    """
+    response = test_client.get("/misc/policy_info")
+
+    soup = BeautifulSoup(response.data, 'html.parser')
+
+    assert response.status_code == 200
+    assert soup.title.string.strip() == page_title_dict["policy_info.html"].strip()  # for simple pages, this suffices
