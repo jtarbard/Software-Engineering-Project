e84f9c9b56830a49d021d3e5c867a8dc986ec7b2
VincentLou <sc18cwl@leeds.ac.uk>
2020-04-02 08:26:38 +0100

    Improve test scenario descriptions for test_login_get_basic(); Create partial tests for test_login_post_basic()

--- tests/functional/account/test_login_register.py
+++ tests/functional/account/test_login_register.py
@@ -43,12 +43,19 @@ def test_login_get_basic(app, test_client, mocker):
     def return_not_logged_in_user_response(request, needs_login):
         return False, flask.redirect("/account/login")
 
+    # ------------------------------------------------------- #
+
     """
+    Login_Get_Basic_Test_1
     GIVEN a Flask application
     WHEN the '/account/login' page is requested (GET)
     UNDER CONDITIONS 1. User not logged in
                      2. No cookies
-    THEN check the page correctly renders (and gives valid response)
+    THEN check 1. Valid status code (200)
+               2. The redirected url is "/account/login"
+               3. page_title (rendered template parameter) or actual page title has "Login"
+               4. '/account/login_register.html' is rendered
+               5. No cookies are created
 
     TESTING FOR <Rule '/account/login' (OPTIONS, HEAD, GET) -> account.login_get>
     """
@@ -59,21 +66,31 @@ def test_login_get_basic(app, test_client, mocker):
         rv = test_client.get('/account/login', follow_redirects=True)
         soup = BeautifulSoup(rv.data, 'html.parser')
 
-        assert "Login" in soup.title.string
+        exp_title = "Login"
+
         assert rv.status_code == 200
+        assert "/account/login" == flask.request.path
         assert len(templates) == 1
         template, context = templates[0]
+        assert exp_title in soup.title.string or exp_title in context.get("page_title", "")
         assert template.name == '/account/login_register.html'
+        assert len(flask.request.cookies) == 0
 
     # ------------------------------------------------------- #
 
     """
+    Login_Get_Basic_Test_2
     GIVEN a Flask application
     WHEN the '/account/login' page is requested (GET)
     UNDER CONDITIONS 1. User not logged in
                      2. Has basket cookies
                      3. Has random cookies
-    THEN check basket cookie is cleared, the page correctly renders (and gives valid response)
+    THEN check 1. Valid status code (200)
+               2. The redirected url is "/account/login"
+               3. page_title (rendered template parameter) or actual page title has "Login"
+               4. '/account/login_register.html' is rendered
+               5. Basket cookie is deleted
+               6. Other random cookies are retained
 
     TESTING FOR <Rule '/account/login' (OPTIONS, HEAD, GET) -> account.login_get>
     """
@@ -87,15 +104,19 @@ def test_login_get_basic(app, test_client, mocker):
         rv = test_client.get('/account/login', follow_redirects=True)
         soup = BeautifulSoup(rv.data, 'html.parser')
 
+        exp_title = "Login"
+
         assert rv.status_code == 200
-        assert "Login" in soup.title.string
+        assert "/account/login" == flask.request.path
+        assert len(templates) == 1
+        template, context = templates[0]
+        assert exp_title in soup.title.string or exp_title in context.get("page_title", "")
+        assert template.name == '/account/login_register.html'
         assert "vertex_basket_cookie" not in flask.request.cookies
         assert "random_cookie7" in flask.request.cookies
         assert "random_cookie11" in flask.request.cookies
-        assert len(templates) == 1
-        template, context = templates[0]
-        assert template.name == '/account/login_register.html'
 
+    # remove cookies so test_client is clean again
     test_client.delete_cookie("localhost", "random_cookie7")
     test_client.delete_cookie("localhost", "random_cookie11")
 
@@ -104,13 +125,16 @@ def test_login_get_basic(app, test_client, mocker):
     mocker.patch('main.view_lib.cookie_lib.return_user_response', side_effect=return_logged_in_user_response)
 
     """
+    Login_Get_Basic_Test_3
     GIVEN a Flask application
     WHEN the '/account/login' page is requested (GET)
     UNDER CONDITIONS 1. User logged in
                      2. No cookies
-    THEN check the page correctly renders (and gives valid response)
-         1. Redirect to index page
-         2. '/index/index.html' gets rendered
+    THEN check 1. Valid status code (200)
+               2. The redirected url is "/" (homepage)
+               3. page_title (rendered template parameter) or actual page title has "Index"
+               4. '/index/index.html' is rendered
+               5. No cookies are created
 
     TESTING FOR <Rule '/account/login' (OPTIONS, HEAD, GET) -> account.login_get>
     """
@@ -120,24 +144,30 @@ def test_login_get_basic(app, test_client, mocker):
         rv = test_client.get("/account/login", follow_redirects=True)
         soup = BeautifulSoup(rv.data, 'html.parser')
 
-        assert 'Index' in soup.title.string
+        exp_title = "Index"
+
         assert rv.status_code == 200
+        assert "/" == flask.request.path
         assert len(templates) == 1
         template, context = templates[0]
+        assert exp_title in soup.title.string or exp_title in context.get("page_title", "")
         assert template.name == '/index/index.html'
-
-    # db_session.add(new_user('customer'))
-    # db_session.commit()
+        assert len(flask.request.cookies) == 0
 
     # ------------------------------------------------------- #
 
     """
+    Login_Get_Basic_Test_4
     GIVEN a Flask application
     WHEN the '/account/login' page is requested (GET)
     UNDER CONDITIONS 1. User logged in
                      2. Basket cookies exist
                      3. Has random cookies
-    THEN check the page correctly renders (and gives valid response)
+    THEN check 1. Valid status code (200)
+               2. The redirected url is "/" (homepage)
+               3. page_title (rendered template parameter) or actual page title has "Index"
+               4. '/index/index.html' is rendered
+               5. All cookies are retained
 
     TESTING FOR <Rule '/account/login' (OPTIONS, HEAD, GET) -> account.login_get>
     """
@@ -148,56 +178,260 @@ def test_login_get_basic(app, test_client, mocker):
         test_client.set_cookie("localhost", "random_cookie17", "this should persist")
 
         rv = test_client.get("/account/login", follow_redirects=True)
-        pprint.pprint(flask.request.cookies)
         soup = BeautifulSoup(rv.data, 'html.parser')
 
+        exp_title = "Index"
+
+        assert rv.status_code == 200
+        assert "/" == flask.request.path
+        assert len(templates) == 1
+        template, context = templates[0]
+        assert exp_title in soup.title.string or exp_title in context.get("page_title", "")
+        assert template.name == '/index/index.html'
         # Shouldn't clear basket cookies (or any other cookies)
         assert "vertex_basket_cookie" in flask.request.cookies
         assert "random_cookie13" in flask.request.cookies
         assert "random_cookie17" in flask.request.cookies
 
-        # Redirects to Index
-        assert 'Index' in soup.title.string
-        assert rv.status_code == 200
-        assert len(templates) == 1
-        template, context = templates[0]
-        assert template.name == '/index/index.html'
+    # --------------------------------- END OF THIS TEST: test_login_get_basic --------------------------------- #
 
 
-# def test_login_get_extra(app, test_client, mocker):
-#     assert False
-
-
-def test_login_post_basic(app, test_client, mocker):
-
-    with conftest.captured_templates(app) as templates:
-        rv = test_client.get('/account/login', follow_redirects=True)
-        soup = BeautifulSoup(rv.data, 'html.parser')
-
-        assert "Login" in soup.title.string
-        assert rv.status_code == 200
-        assert len(templates) == 1
-        template, context = templates[0]
-        assert template.name == '/account/login_register.html'
+def test_login_post_basic(app, test_client, mocker, new_user):
 
     """
-    GIVEN a Flask application
-    WHEN data form is submitted to the '/account/login' page (POST)
-    UNDER CONDITIONS ... TBA
-    THEN check the response is valid
-
-    TESTING FOR <Rule '/account/login' (OPTIONS, POST) -> account.login_post>
+    PRELIMINARY DATABASE CONDITIONS:
+    + Customer = new_user
+    + Employee = new_user
+    + Manager = new_user
     """
-
-    mocker.patch('main.view_lib.cookie_lib.return_user_response', side_effect=return_not_logged_in_user_response)
-
-    assert False
+    from main.data.db_session import add_to_database, delete_from_database
+    add_to_database(new_user("customer"))
+    add_to_database(new_user("employee"))
+    add_to_database(new_user("manager"))
 
     # ------------------------------------------------------- #
 
     """
-    more test scenarios
+    Login_Post_Basic_Test_1
+    GIVEN a Flask application
+    WHEN data form is submitted to the '/account/login' page (POST)
+    UNDER CONDITIONS 1. user exists in database
+                     2. existent email
+                     3. existent password
+                     4. Password length is 8-15 and has no invalid characters
+                     5. email and password are matching
+    THEN check 1. Valid status code (200)
+               2. Redirected route is "account.view_account" (the function called is view_account())
+               3. page_title or _main_layout title has "Your Account"
+               4. "account/account.html" is rendered
+
+    TESTING FOR <Rule '/account/login' (OPTIONS, POST) -> account.login_post>
     """
+
+    with conftest.captured_templates(app) as templates:
+        rv = test_client.post('/account/login', data=dict(email="johndoe@thevertex.com",
+                                                          password="passw0rD"), follow_redirects=True)
+        soup = BeautifulSoup(rv.data, 'html.parser')
+
+        exp_title = "Your Account"
+
+        assert rv.status_code == 200
+        assert flask.url_for("account.view_account") == flask.request.path
+        assert len(templates) == 1
+        template, context = templates[0]
+        assert exp_title in soup.title.string or exp_title in context.get("page_title", "")
+        assert template.name == '/account/account.html'
+
+    # ------------------------------------------------------- #
+
+    """
+    Login_Post_Basic_Test_2
+    GIVEN a Flask application
+    WHEN data form is submitted to the '/account/login' page (POST)
+    UNDER CONDITIONS 1. user exists in database
+                     2. existent email
+                     3. existent password
+                     4. Password length is 8-15 and has no invalid characters
+                     5. email and password are NOT matching
+    THEN check 1. Valid status code (200)
+               2. Redirected url is "/account/login"
+               3. page_title or _main_layout title has "Login"
+               4. "account/login_register.html" is rendered
+               5. Email is retained
+               6. ServerError should describe email and password are NOT matching (Currently, "Input error: Incorrect email or password")
+
+    TESTING FOR <Rule '/account/login' (OPTIONS, POST) -> account.login_post>
+    """
+
+    with conftest.captured_templates(app) as templates:
+        rv = test_client.post('/account/login', data=dict(email="johndoe@thevertex.com",
+                                                          password="Admin666"), follow_redirects=True)
+        soup = BeautifulSoup(rv.data, 'html.parser')
+
+        exp_title = "Login"
+
+        assert rv.status_code == 200
+        assert "/account/login" == flask.request.path
+        assert len(templates) == 1
+        template, context = templates[0]
+        assert exp_title in soup.title.string or exp_title in context.get("page_title", "")
+        assert template.name == '/account/login_register.html'
+
+        assert "johndoe@thevertex.com" == context.get("email", "")
+        assert "Input error: Incorrect email or password" == context.get("ServerError", "")
+
+    # ------------------------------------------------------- #
+
+    """
+    Login_Post_Basic_Test_3
+    GIVEN a Flask application
+    WHEN data form is submitted to the '/account/login' page (POST)
+    UNDER CONDITIONS 1. when user exists in database
+                     2. existent email
+                     3. NON-EXISTENT password
+                     4. Password length is 8-15 and has no invalid characters
+                     5. email and password are NOT matching
+    THEN check 1. Valid status code (200)
+               2. Redirected url is "/account/login"
+               3. page_title or _main_layout title has "Login"
+               4. "account/login_register.html" is rendered
+               5. Email is retained
+               6. ServerError should describe email and password are NOT matching (Currently, "Input error: Incorrect email or password")
+
+    TESTING FOR <Rule '/account/login' (OPTIONS, POST) -> account.login_post>
+    """
+
+
+
+    # ------------------------------------------------------- #
+
+    """
+    Login_Post_Basic_Test_4
+    GIVEN a Flask application
+    WHEN data form is submitted to the '/account/login' page (POST)
+    UNDER CONDITIONS 1. when user exists in database
+                     2. NON-EXISTENT email
+                     3. existent password
+                     4. Password length is 8-15 and has no invalid characters
+                     5. email and password are NOT matching
+    THEN check 1. Valid status code (200)
+               2. Redirected url is "/account/login"
+               3. page_title or _main_layout title has "Login"
+               4. "account/login_register.html" is rendered
+               5. Email is retained
+               6. ServerError should describe email is not found (Currently, "Input error: Incorrect email or password")
+
+    TESTING FOR <Rule '/account/login' (OPTIONS, POST) -> account.login_post>
+    """
+
+
+
+    # ------------------------------------------------------- #
+
+    """
+    Login_Post_Basic_Test_5
+    GIVEN a Flask application
+    WHEN data form is submitted to the '/account/login' page (POST)
+    UNDER CONDITIONS 1. when user exists in database
+                     2. NON-EXISTENT email
+                     3. NON-EXISTENT password
+                     4. Password length is 8-15 and has no invalid characters
+                     5. email and password are NOT matching
+    THEN check 1. Valid status code (200)
+               2. Redirected url is "/account/login"
+               3. page_title or _main_layout title has "Login"
+               4. "account/login_register.html" is rendered
+               5. Email is retained
+               6. ServerError should describe email is not found (Currently, "Input error: Incorrect email or password")
+
+    TESTING FOR <Rule '/account/login' (OPTIONS, POST) -> account.login_post>
+    """
+
+
+
+    # ------------------------------------------------------- #
+
+    """
+    Login_Post_Basic_Test_6
+    GIVEN a Flask application
+    WHEN data form is submitted to the '/account/login' page (POST)
+    UNDER CONDITIONS 1. when user exists in database
+                     2. Existent email
+                     3. NON-EXISTENT password
+                     4. Password length OUT OF 8-15 range
+                     5. Password has no invalid characters
+                     6. email and password are (naturally) NOT matching
+    THEN check 1. Valid status code (200)
+               2. Redirected url is "/account/login"
+               3. page_title or _main_layout title has "Login"
+               4. "account/login_register.html" is rendered
+               5. Email is retained
+               6. ServerError should describe password is of wrong size (Currently, "Input error: password is not of correct size (8-15)")
+
+    TESTING FOR <Rule '/account/login' (OPTIONS, POST) -> account.login_post>
+    """
+
+
+
+    # ------------------------------------------------------- #
+
+    """
+    Login_Post_Basic_Test_7
+    GIVEN a Flask application
+    WHEN data form is submitted to the '/account/login' page (POST)
+    UNDER CONDITIONS 1. when user exists in database
+                     2. Existent email
+                     3. NON-EXISTENT password
+                     4. Password length is 8-15
+                     5. Password HAS INVALID characters
+                     6. email and password are (naturally) NOT matching
+    THEN check 1. Valid status code (200)
+               2. Redirected url is "/account/login"
+               3. page_title or _main_layout title has "Login"
+               4. "account/login_register.html" is rendered
+               5. Email is retained
+               6. ServerError should describe password contains invalid characters (Currently, "Input Error: Password not in valid format")
+
+    TESTING FOR <Rule '/account/login' (OPTIONS, POST) -> account.login_post>
+    """
+
+
+
+    # ------------------------------------------------------- #
+
+    """
+    Login_Post_Basic_Test_8
+    GIVEN a Flask application
+    WHEN data form is submitted to the '/account/login' page (POST)
+    UNDER CONDITIONS 1. when user exists in database
+                     2. Existent email
+                     3. NON-EXISTENT password
+                     4. Password length is OUT OF 8-15 range
+                     5. Password HAS INVALID characters
+                     6. email and password are (naturally) NOT matching
+    THEN check 1. Valid status code (200)
+               2. Redirected url is "/account/login"
+               3. page_title or _main_layout title has "Login"
+               4. "account/login_register.html" is rendered
+               5. Email is retained
+               6. ServerError should describe either: password is of wrong size (Currently, "Input error: password is not of correct size (8-15)"), or
+                                                      password contains invalid characters (Currently, "Input Error: Password not in valid format")
+
+    TESTING FOR <Rule '/account/login' (OPTIONS, POST) -> account.login_post>
+    """
+
+
+
+    # TODO: Add 3 more tests: Non-Existent email, invalid password format -> should throw error says invalid password without connecting to database.
+
+    # --------------------------------- END OF THIS TEST: test_login_post_basic --------------------------------- #
+
+
+# def test_login_post_extra():
+#     # Empty email. maybe can test in basic?
+#     # Weird email format - e.g. no @domain.[com]
+#     # Unicode in data form
+#     # SQL Injection :)
 
 
 def test_logout_get_basic(test_client):
